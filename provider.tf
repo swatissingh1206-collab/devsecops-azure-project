d869a97f8044760872d4f8663a7a16d00b020696
swati <swati.singh@synlabs.io>
2026-06-02 15:38:36 +0530

    .\provider.tf

--- provider.tf
+++ provider.tf
@@ -1,13 +1,13 @@
 terraform {
-	required_provider {
-	azurerm = {
-		source = "hashicorp/azurerm"
-	version = "4.72.0"		
-}
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "4.72.0"
+    }
 
-}
+  }
 }
 
 provider "azurerm" {
-	features {}
+  features {}
 }
