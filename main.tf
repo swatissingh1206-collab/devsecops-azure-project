fcebd86f89f6e9403fe6d7783398a458758b80b4
swati <swati.singh@synlabs.io>
2026-06-02 14:24:40 +0530

    .\main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,9 @@
-resource "azurerm_resource_group" "mainblockrg"
-{
-  name = "rgmain123"
+resource "azurerm_resource_group" "mainblockrg" {
+  name     = "rgmain123"
   location = "East us"
-  }
+}
+
+resource "azurerm_resource_group" "mainblockrg1" {
+  name     = "rgmain1234"
+  location = "East us"
+}
